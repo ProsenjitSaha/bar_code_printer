f31d6bb8edf1e5c2731e93b9bc0aa701c70fcf3b
Santanu Bhattacharya <sbhattacharya@kreeti.com>
2013-11-01 15:19:02 +0530

    refs, #kreeti-5, new-show page added

--- app/controllers/bar_code_printer/bar_codes_controller.rb
+++ app/controllers/bar_code_printer/bar_codes_controller.rb
@@ -10,5 +10,28 @@ module BarCodePrinter
       @bar_code_image = bar_code.to_png
       render :show, layout: false
     end
+
+    def new_show
+      @bar_code_value = params[:id] if params[:id]
+      @first_line_text = params[:first_line_text] || ""
+      @second_line_text = params[:second_line_text] || ""
+
+      if @bar_code_value
+        bar_code = Barby::Code39.new(@bar_code_value)
+        @bar_code_image = bar_code.to_png
+      end
+
+      if params[:item_id]
+        bar_code_item = Barby::Code39.new(params[:item_id])
+        @item_code_image = bar_code_item.to_png if bar_code_item
+      end
+
+      if params[:consignment_id]
+        bar_code_consignment = Barby::Code39.new(params[:consignment_id])
+        @consignment_code_image = bar_code_consignment.to_png
+      end
+
+      render :new_show, layout: false
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 BarCodePrinter::Engine.routes.draw do
-  resources :bar_codes, only: [:show]
+  resources :bar_codes, only: [:show] do 
+    get :new_show, on: :collection
+  end
 end
