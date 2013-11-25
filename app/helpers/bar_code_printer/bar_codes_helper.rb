5f6fa2dfca5b86fe13b4f5d8d7e8008e6f86adc2
Santanu Bhattacharya <sbhattacharya@kreeti.com>
2013-11-25 10:53:48 +0530

    only batch name is shown in title

--- app/helpers/bar_code_printer/bar_codes_helper.rb
+++ app/helpers/bar_code_printer/bar_codes_helper.rb
@@ -6,13 +6,5 @@ module BarCodePrinter
       bar_code_image = bar_code.to_png
       image_tag("data:image/png;base64,#{Base64.encode64(bar_code_image)}", class: "barcode_icon")
     end
-
-    def master_picklist_title(text)
-      str = ""
-      if text
-        text.first["name"].present? ? JSON.parse(text.first["name"]).each { |key, value| str << (key.capitalize + ": " + value.capitalize + "  ")} : (str << "Order ID: " + text.first["order_id"])
-      end
-      str
-    end
   end
 end
