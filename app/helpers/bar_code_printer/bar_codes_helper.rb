6763220118d7a8162f44b4c1e22044c8b9599dec
Santanu Bhattacharya <sbhattacharya@kreeti.com>
2013-12-03 15:21:07 +0530

    packing list html modified

--- app/helpers/bar_code_printer/bar_codes_helper.rb
+++ app/helpers/bar_code_printer/bar_codes_helper.rb
@@ -4,7 +4,12 @@ module BarCodePrinter
     def bar_code_image(text)
       bar_code = Barby::Code39.new(text)
       bar_code_image = bar_code.to_png
-      image_tag("data:image/png;base64,#{Base64.encode64(bar_code_image)}", class: "barcode_icon")
+      image_tag("data:image/png;base64,#{Base64.encode64(bar_code_image)}", class: "barcode-icon")
+    end
+
+    def time_format(time)
+      t = time.to_date
+      t.strftime("%m/%d/%Y") + " " + t.strftime("at %I:%M%p") if t
     end
   end
 end
